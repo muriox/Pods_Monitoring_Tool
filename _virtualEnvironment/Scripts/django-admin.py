58dcdca460fd8729c06cd7a819e76fe6b1465a55
Mudashiru <murio_x@yahoo.com>
2019-08-02 16:43:37 +0100

    Remove debug virtual environment

--- _virtualEnvironment/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\muda's\documents\codes\python\django\_virtualenvironment\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
