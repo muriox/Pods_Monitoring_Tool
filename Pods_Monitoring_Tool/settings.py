8ed39761aa079aff01de2841ff083b284cbc0291
Mudashiru <murio_x@yahoo.com>
2019-08-02 17:59:28 +0100

    static file path change

--- Pods_Monitoring_Tool/settings.py
+++ Pods_Monitoring_Tool/settings.py
@@ -121,7 +121,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join('D:/home/site/wwwroot/static', "static")
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # NOTE: The URL path is renamed to hide the actual path to static files
 STATIC_URL = '/uadsshAFSVAasb/'
 
