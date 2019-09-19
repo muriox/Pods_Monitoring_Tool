5e6e9dce2988f798f82351bbe5a78420dac84f8c
Mudashiru <murio_x@yahoo.com>
2019-09-20 00:24:27 +0100

    Page check correction

--- monitoringTool/static/monitoringTool/js/entryChecks.js
+++ monitoringTool/static/monitoringTool/js/entryChecks.js
@@ -1,8 +1,43 @@
-// Run Timeout session
-timeOutSession('expires');
+// Active status check
+checkActiveStatusWithSession('expires');
+
+// Checks active status using session life spam
+function checkActiveStatusWithSession(expires) {
+    // Expiring time
+    var expiresTime = sessionStorage.getItem(expires);
+
+    // Current time
+	var now = new Date();
+
+	if (now.getHours() != 0 || now.getMinutes() != 0) {
+		now = now.getMinutes() * now.getHours();
+	} else {
+		now = now.getMinutes();
+	}
+
+    var diff =  expiresTime - now;
+    //console.log("**now: " + now);
+    console.log("**expiresTime: " + expiresTime);
+    //console.log("**Diff: expiresTime - now: " + diff);
+
+	if ((now > expiresTime) || (expiresTime == "" || expiresTime == null)) {
+	    console.log("Time for redirect");
+	    sessionStorage.clear();
+
+		var newPage = "https://podsmanagementapp.azurewebsites.net/";
+		window.location.replace(newPage); // Redirect
+	}
+
+	// Timer
+	setTimeout(function(){
+		checkActiveStatusWithSession(expires); // Recall function
+	}, 15000);
+}
 
 // Timeout session method
-function timeOutSession(expires) {
+function timeOutSession() {
+	console.log("timeOutSession()");
+
     // Expiring time (sec)
     var expiresIn = 1800000;
 	var homePage = "https://podsmanagementapp.azurewebsites.net/";
