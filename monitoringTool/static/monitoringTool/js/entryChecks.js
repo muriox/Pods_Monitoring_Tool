a272b9e98764647ab56e8687b0edec2f3fbe69e6
Mudashiru <murio_x@yahoo.com>
2019-09-19 09:59:29 +0100

    Session timeout

--- monitoringTool/static/monitoringTool/js/entryChecks.js
+++ monitoringTool/static/monitoringTool/js/entryChecks.js
@@ -1,47 +1,28 @@
-// Active status check
-checkActiveStatusWithSession('expires');
+// Run Timeout session
+timeOutSession('expires');
 
-// Checks active status using session life spam
-function checkActiveStatusWithSession(expires) {
-    // Expiring time
-    var expiresTime = sessionStorage.getItem(expires);
-
-    // Current time
-	var now = new Date();
-
-	if (now.getHours() != 0 || now.getMinutes() != 0) {
-		now = now.getMinutes() * now.getHours();
-	} else {
-		now = now.getMinutes();
-	}
-
-    var diff =  expiresTime - now;
-    console.log("**now: " + now);
-    console.log("**expiresTime: " + expiresTime);
-    console.log("**Diff: expiresTime - now: " + diff);
-
-	if ((now > expiresTime) || (expiresTime == "" || expiresTime == null)) {
-	    console.log("Time for redirect");
-	    sessionStorage.clear();
-
-		var newPage = "http://127.0.0.1:8000/";
-		window.location.replace(newPage); // Redirect
-	}
+// Timeout session method
+function timeOutSession(expires) {
+    // Expiring time (sec)
+    var expiresIn = 1800000;
+	var homePage = "https://podsmanagementapp.azurewebsites.net/";
 
 	// Timer
 	setTimeout(function(){
-		checkActiveStatusWithSession(expires); // Recall function
-	}, 15000);
+		console.log("Time for redirect");
+		sessionStorage.clear();
+	
+		window.location.replace(homePage); // Redirect		
+	}, expiresIn);
 }
 
 
-// Checks active status using cookie life spam
+// TEST: Checks active status using cookie life spam
 function checkActiveStatusWithCookie(username) {
 
 	alert('Before');
 	var user = getCookie(username);
-	//var user = getCookie(username);
-	//console.log('Checking cookie...' + user);
+
 	if (user = "" || user == null) {
 		//var newPage = "https://" + window.location.host;
 		var newPage = "https://www.google.com";
@@ -56,4 +37,3 @@ function checkActiveStatusWithCookie(username) {
 		checkActiveStatus(username); // Recall function
 	}, 5000);
 }
-//    });
